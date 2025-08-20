9b1411c989a3634bee64bc18e902f90bef570d9d
Debjyoti Shit <debyotishit27@gmail.com>
2025-08-21 02:25:30 +0530

    Added the docker and docker compose files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.json(
+        { 
+            author: "Debjyoti",
+            status: "success",
+            message: "Hello, World! Your CI/CD Pipeline is working !!"
+        }
+    )
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
